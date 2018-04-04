05ad1aa145596735ea15f5096f4b697839d29f58
Shintaro Iwasaki <siwasaki@anl.gov>
2018-04-04 11:01:17 -0500

    Implement master_worker_dynamic.c.

---
+++ exercises/blocking_p2p/master_worker_dynamic.c
@@ -0,0 +1,77 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    int rank, count, size;
+    int data[100];
+    MPI_Status status;
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    srand(time(NULL));
+
+    /* all processes except the last one are workers */
+    if (rank < size - 1) {
+        while (1) {
+            int data_count;
+            /* send request to master */
+            MPI_Send(data, 0, MPI_INT, size - 1, 0, MPI_COMM_WORLD);
+            /* receive data from master */
+            MPI_Recv(data, 100, MPI_INT, size - 1, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            MPI_Get_count(&status, MPI_INT, &data_count);
+            if (data_count == 0) {
+                /* received termination request */
+                break;
+            } else {
+                /* received work */
+                int task_id = status.MPI_TAG;
+                sleep(1);       /* compute data */
+                /* send data to master */
+                MPI_Send(data, data_count, MPI_INT, size - 1, task_id, MPI_COMM_WORLD);
+            }
+        }
+    } else {
+        /* master process */
+        /* send data to workers. work size is 1 */
+        int nprocessed = 0;     /* number of processed work units */
+        int nterminated = 0;    /* number of terminated workers */
+        int recv_buffer[100];
+        while (nterminated < size - 1) {
+            /* receive request from worker */
+            MPI_Recv(recv_buffer, 100, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD,
+                     &status);
+            MPI_Get_count(&status, MPI_INT, &count);
+            if (count == 0) {
+                /* worker is idle */
+                if (nprocessed < 100) {
+                    /* send work to worker */
+                    int task_id = nprocessed;
+                    int data_count = 1 + rand() % 4;
+                    if (nprocessed + data_count > 100)
+                        data_count = 100 - nprocessed;
+                    MPI_Send(data + nprocessed, data_count, MPI_INT, status.MPI_SOURCE, task_id,
+                             MPI_COMM_WORLD);
+                    nprocessed += data_count;
+                } else {
+                    /* send termination request to worker because all work has been processed */
+                    MPI_Send(NULL, 0, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
+                    nterminated++;
+                }
+            } else {
+                /* show result computed by worker */
+                printf("worker ID: %d; task ID: %d; count: %d\n", status.MPI_SOURCE, status.MPI_TAG,
+                       count);
+            }
+        }
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Finalize();
+    return 0;
+}
