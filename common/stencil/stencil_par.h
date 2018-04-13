58fa1bb36e0711ffd3bfa945d7e321940ac061de
Shintaro Iwasaki <siwasaki@anl.gov>
2018-04-13 17:45:52 -0500

    [stencil] Add stencil_alltoallw.c
    Use MPI_Alltoallw and derived data types.
    - Add (...) to the macro in stencil_par.h, which is error-prone.

--- common/stencil/stencil_par.h
+++ common/stencil/stencil_par.h
@@ -16,7 +16,7 @@
 #include <stdint.h>
 
 /* row-major order */
-#define ind(i,j) (j)*(bx+2)+(i)
+#define ind(i,j) ((j)*(bx+2)+(i))
 
 void printarr_par(int iter, double *array, int size, int px, int py, int rx, int ry, int bx, int by,
                   int offx, int offy, MPI_Comm comm);
